1a3f595289078f6d819dfa851503151eed652aeb
Leonid Khlebushchev <lowpold1@gmail.com>
2022-05-01 11:14:59 +0300

    [src][res] Config changes. ubus call implementation. Rewrite module for using schedule library correctly

--- reports.py
+++ reports.py
@@ -25,10 +25,6 @@ class period_type(enum.Enum):
     monthly = 4
 
 class report:
-    #def __run(self):
-    #    journal.WriteLog(module_name, "Normal", "notice", "Report callbacks: " + str(self.__callbacks))
-
-        
     def __init__(self, name, description, active, callbacks, method, period, report_format, settings):
         self.__name = name
         self.__description = description
@@ -41,13 +37,27 @@ class report:
 
         def run():
             journal.WriteLog(module_name, "Normal", "notice", "Report callbacks: " + str(self.__callbacks))
-            #TODO ubus call for getting parameters
+            #ubus call for getting parameters
+            callbacks = self.__callbacks
+            messages = []
+            for c in callbacks:
+                result = ubus.call(c['module'], c['method'], c['input_parameters'])
+
+                if not result:
+                    journal.WriteLog(module_name, "Normal", "error", "Can't get ubus call for " + c['module'] + " with method " + c['method'])
+                    continue
+
+                m = { 
+                        "name" : c['module'],
+                        "result" : result
+                    }
+                messages.append(m)
             #TODO generate text from report_format
             text = report_format
 
             if self.__method == method_type.email:
-            for addr in self.__settings['toaddr']:
-                ubus.call("owrt_email", "send_mail", { "fromaddr":self.__settings['fromaddr'], "toaddr":addr, "text": text, "subject":self.__settings['subject'] ,"signature":self.__settings['signature'], "ubus_rpc_session":"1" })
+                #for addr in self.__settings['toaddr']:
+                #    ubus.call("owrt_email", "send_mail", { "fromaddr":self.__settings['fromaddr'], "toaddr":addr, "text": text, "subject":self.__settings['subject'] ,"signature":self.__settings['signature'], "ubus_rpc_session":"1" })
 
 
         if self.__active:
@@ -78,9 +88,7 @@ period_type_map = {
                 }
 
 mutex = Lock()
-pollThread = None
 ubusConnected = False
-poll_flag = True
 default_report = None
 max_reports = 20
 
@@ -96,87 +104,115 @@ def reconfigure(event, data):
 
         applyconfig()
 
-def poll():
-    global poll_flag
-
-    while poll_flag:
-        journal.WriteLog(module_name, "Normal", "notice", "run_pending")
-        schedule.run_pending()
-        time.sleep(1)
-
 def applyconfig():
-    global pollThread
     global ubusConnected
 
     try:
         if not ubusConnected:
             ubus.connect()
 
+        default_name = None
+        default_active = None
+        default_description = None
+        default_callbacks = None
+        default_method = None
+        default_period = None
+        default_report_format = None
+        default_settings = None
+
         confvalues = ubus.call("uci", "get", {"config": confName})
         for confdict in list(confvalues[0]['values'].values()):
             if confdict['.type'] == 'report' and confdict['.name'] == 'prototype':
-                name = confdict['name']
-                active = bool(int(confdict['state']))
-                description = confdict['description']
-                callbacks = json.loads(confdict['callbacks'])
-                method = method_type_map[confdict['method']]
-                period = confdict['schedule']
-                report_format = confdict['text']
-                settings = {}
+                default_name = confdict['name']
+                default_active = bool(int(confdict['state']))
+                default_description = confdict['description']
+                default_callbacks = json.loads(confdict['callbacks'])
+                default_method = method_type_map[confdict['method']]
+                default_period = confdict['schedule']
+                default_report_format = confdict['text']
+                default_settings = {}
 
                 #settings parse
-                if method == method_type.empty:
+                if default_method == method_type.empty:
                     pass
-                elif method == method_type.email:
+                elif default_method == method_type.email:
                     try:
-                        settings['fromaddr'] = confdict['from']
+                        default_settings['fromaddr'] = confdict['from']
                     except:
-                        settings['fromaddr'] = ''
+                        default_settings['fromaddr'] = ''
 
                     try:
-                        settings['subject'] = confdict['subject']
+                        default_settings['subject'] = confdict['subject']
                     except:
-                        settings['subject'] = ''
+                        default_settings['subject'] = ''
 
                     try:
-                        settings['signature'] = confdict['signature']
+                        default_settings['signature'] = confdict['signature']
                     except:
-                        settings['signature'] = ''
+                        default_settings['signature'] = ''
 
                     try:
-                        settings['toaddr'] = [ a for a in confdict['sendto'].split(',')]
+                        default_settings['toaddr'] = [ a for a in confdict['sendto'].split(',')]
                     except:
-                        settings['toaddr'] = []
-                elif method == method_type.snmptrap:
+                        default_settings['toaddr'] = []
+                elif default_method == method_type.snmptrap:
                     try:
-                        settings['url'] = confdict['url']
+                        default_settings['url'] = confdict['url']
                     except:
-                        settings['url'] = ''
+                        default_settings['url'] = ''
 
                     try:
-                        settings['oid'] = confdict['oid']
+                        default_settings['oid'] = confdict['oid']
                     except:
-                        settings['oid'] = ''
+                        default_settings['oid'] = ''
 
                     try:
-                        settings['port'] = confdict['port']
+                        default_settings['port'] = confdict['port']
                     except:
-                        settings['port'] = ''
+                        default_settings['port'] = ''
                 else:
                     journal.WriteLog(module_name, "Normal", "error", "Wrong method type. Settings is empty")
 
-                default_report = report(name, description, active, callbacks, method, period, report_format, settings)
+                #default_report = report(name, description, active, callbacks, method, period, report_format, settings)
 
             if confdict['.type'] == 'report' and confdict['.name'] != 'prototype':
                 exist = False
 
-                name = confdict['name']
-                active = bool(int(confdict['state']))
-                description = confdict['description']
-                callbacks = json.loads(confdict['callbacks'])
-                method = method_type_map[confdict['method']]
-                period = confdict['schedule']
-                report_format = confdict['text']
+                try:
+                    name = confdict['name']
+                except:
+                    name = default_name
+
+                try:
+                    active = bool(int(confdict['state']))
+                except:
+                    active = default_active
+
+                try:
+                    description = confdict['description']
+                except:
+                    description = default_description
+
+                try:
+                    callbacks = json.loads(confdict['callbacks'])
+                except:
+                    callbacks = default_callbacks
+
+                try:
+                    method = method_type_map[confdict['method']]
+                except:
+                    method = default_method
+
+                try:
+                    period = confdict['schedule']
+                except:
+                    period = default_period
+
+                try:
+                    report_format = confdict['text']
+                except:
+                    report_format = default_report
+
                 settings = {}
 
                 #settings parse
@@ -216,7 +252,8 @@ def applyconfig():
                     except:
                         settings['port'] = ''
                 else:
-                    journal.WriteLog(module_name, "Normal", "error", "Wrong method type. Settings is empty")
+                    settings = default_settings
+                    journal.WriteLog(module_name, "Normal", "error", "Wrong method type. Settings is default")
 
                 r = report(name, description, active, callbacks, method, period, report_format, settings)
                 
@@ -237,20 +274,21 @@ def applyconfig():
         journal.WriteLog(module_name, "Normal", "error", "Can't connect to ubus " + str(ex))
 
 if __name__ == "__main__":
+    journal.WriteLog(module_name, "Normal", "notice", "Module " + module_name + " started!")
+
     try:
         applyconfig()
 
-        if not pollThread:
-            pollThread = Thread(target=poll, args=())
-            pollThread.start()
-
         ubus.connect()
 
-        ubus.listen(("commit", reconfigure))
-        ubus.loop()
+        while True:
+            schedule.run_pending()
+            ubus.listen(("commit", reconfigure))
+            ubus.loop(1)
 
     except KeyboardInterrupt:
         del reports[:]
-        poll_flag = False
 
     ubus.disconnect()
+
+    journal.WriteLog(module_name, "Normal", "notice", "Module " + module_name + " finished!")
